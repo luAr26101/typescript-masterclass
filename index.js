7cbbe33b7968d9acdc8af859172f55b8eb0131cf
luAr26101 <raul.cosmin.26@icloud.com>
2025-11-19 08:40:06 +0200

    Complete Typescript introduction section.

---
+++ index.js
@@ -0,0 +1,7 @@
+function addNumbers(a, b) {
+    return a + b;
+}
+console.log(addNumbers(2, 3));
+console.log(addNumbers(2, 2));
+export {};
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,6 @@
+function addNumbers(a: number, b: number) {
+  return a + b;
+}
+
+console.log(addNumbers(2, 3));
+console.log(addNumbers(2, 2));
